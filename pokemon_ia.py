ac3fe7cf6ce34f9a87bca2b21edf216157f64e3a
ByStirith <benoit.ferrere11@gmail.com>
2023-12-01 10:33:02 +0400

    Add Pokemon data processing code

--- pokemon_ia.py
+++ pokemon_ia.py
@@ -1,1 +1,14 @@
-#BA
+import pandas as pa
+import numpy as np
+import os
+
+listeFichiers = os.listdir("C:/Users/benoi/OneDrive/Bureau/Cours/IA_Avec_Pokemon/IA_Avec_Pokemon/datas")
+
+nosPokemons = pa.read_csv("C:/Users/benoi/OneDrive/Bureau/Cours/IA_Avec_Pokemon/IA_Avec_Pokemon/datas/pokedex.csv")
+
+
+
+print(nosPokemons.columns.values)
+nosPokemons['LEGENDAIRE'] = (nosPokemons['LEGENDAIRE'] == "VRAI").astype(int)
+nosPokemons["NOM"][62] = "Colossinge"
+print(nosPokemons["NOM"][62])
